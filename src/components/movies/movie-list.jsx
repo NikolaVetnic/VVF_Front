1c45d834d6c4d8b6b1bbf7cac61fde25f47c096b
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2022-07-15 23:31:50 +0200

    #CSDH-68 implement search movies by title

--- src/components/movies/movie-list.jsx
+++ src/components/movies/movie-list.jsx
@@ -1,36 +1,69 @@
-import React, { useEffect } from "react";
-import { Col, Row } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Col, Form, Row } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { getMovies } from "../../store/movie/actions";
 import { moviesSelector } from "../../store/movie/selectors";
 import { MovieCard } from "./movie-card";
 
+const timeToWaitBeforeSearching = 750;
+
 export const MovieList = () => {
     const dispatch = useDispatch();
     const movies = useSelector(moviesSelector);
+    const [inputValue, setInputValue] = useState("");
+    const [searchTerm, setSearchTerm] = useState("");
 
-    // dispatch(getMovies());
+    useEffect(() => {
+        const delayDebounceFn = setTimeout(() => {
+            setSearchTerm(inputValue);
+        }, timeToWaitBeforeSearching);
+        return () => clearTimeout(delayDebounceFn);
+    }, [inputValue]);
 
     useEffect(() => {
         dispatch(getMovies());
-    }, []);
-    // console.log(movies);
+    }, [dispatch]);
 
     if (movies !== undefined) {
         return (
             <div>
-                <Row xs={1} md={3} className="g-4">
-                    {movies.map((movie, idx) => (
-                        <Col key={idx}>
-                            <MovieCard
-                                id={movie.id}
-                                title={movie.title}
-                                description={movie.description}
-                                imageUrl={movie.imageUrl}
-                                genre={movie.genre}
-                            />
-                        </Col>
-                    ))}
+                <Row>
+                    <Form className="d-flex">
+                        <Form.Control
+                            type="search"
+                            placeholder="Search movies by title"
+                            className="me-2"
+                            aria-label="Search"
+                            value={inputValue}
+                            onChange={(e) => setInputValue(e.target.value)}
+                        />
+                    </Form>
+                </Row>
+                <Row
+                    xs={1}
+                    md={3}
+                    className="g-4"
+                    style={{ marginTop: "0rem" }}
+                >
+                    {movies
+                        .filter((movie) =>
+                            searchTerm === ""
+                                ? true
+                                : movie.title
+                                      .toLowerCase()
+                                      .includes(searchTerm.toLowerCase())
+                        )
+                        .map((movie, idx) => (
+                            <Col key={idx}>
+                                <MovieCard
+                                    id={movie.id}
+                                    title={movie.title}
+                                    description={movie.description}
+                                    imageUrl={movie.imageUrl}
+                                    genre={movie.genre}
+                                />
+                            </Col>
+                        ))}
                 </Row>
             </div>
         );
